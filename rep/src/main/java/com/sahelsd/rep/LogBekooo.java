d5e31b8c167405836d924b3a8a67384055f7ed28
modatherabdelgadermodather <modatherabdelgader@gmail.com>
2018-09-12 00:39:18 +0200

    new edit

---
+++ rep/src/main/java/com/sahelsd/rep/LogBekooo.java
@@ -0,0 +1,12 @@
+package com.sahelsd.rep;
+
+import android.util.Log;
+
+public class LogBekooo {
+
+    private static final String TAG = "LogBekooo";
+
+    public void bekooo(String xxx) {
+        Log.e(TAG, xxx);
+    }
+}
