ae0e099cc50a2d939b61fd707e40531e7c54effe
modatherabdelgadermodather <modatherabdelgader@gmail.com>
2018-09-12 02:20:33 +0200

    add method 2

--- rep/src/main/java/com/sahelsd/rep/LogBekooo.java
+++ rep/src/main/java/com/sahelsd/rep/LogBekooo.java
@@ -9,4 +9,10 @@ public class LogBekooo {
     public static void bekooo(String xxx) {
         Log.e(TAG, xxx);
     }
+
+    public static int bekooo2(int xxx) {
+        int x = xxx;
+        int res = x * 5;
+        return res;
+    }
 }
