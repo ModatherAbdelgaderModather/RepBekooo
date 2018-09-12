3c904ba9700d0e2e5876e1952d97f3ea596d6b0b
modatherabdelgadermodather <modatherabdelgader@gmail.com>
2018-09-12 02:15:48 +0200

    new edit

--- rep/src/main/java/com/sahelsd/rep/LogBekooo.java
+++ rep/src/main/java/com/sahelsd/rep/LogBekooo.java
@@ -6,7 +6,7 @@ public class LogBekooo {
 
     private static final String TAG = "LogBekooo";
 
-    public void bekooo(String xxx) {
+    public static void bekooo(String xxx) {
         Log.e(TAG, xxx);
     }
 }
